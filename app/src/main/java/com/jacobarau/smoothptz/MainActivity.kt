267ba323ebf9b317a99c51ad4fd484d6c9f703ce
Jacob Rau <jarau@mtu.edu>
2021-10-03 13:28:03 -0400

    working exoplayer implementation except it doesn't work with the cameras

--- app/src/main/java/com/jacobarau/smoothptz/MainActivity.kt
+++ app/src/main/java/com/jacobarau/smoothptz/MainActivity.kt
@@ -2,26 +2,85 @@ package com.jacobarau.smoothptz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import android.widget.TextView
+import androidx.core.content.edit
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.ui.StyledPlayerView
 
 
 class MainActivity : AppCompatActivity() {
+    fun play(url: String, player: SimpleExoPlayer) {
+        val mediaItem: MediaItem = MediaItem.fromUri(url)
+        player.setMediaItem(mediaItem)
+        player.prepare()
+        player.play()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val tv: TextView = findViewById(R.id.tv)
         tv.text = HelloJNI().foo.toString()
 
-        val player = SimpleExoPlayer.Builder(this).build()
-        val playerView: StyledPlayerView = findViewById(R.id.player)
-        playerView.player = player
+        val player1 = SimpleExoPlayer.Builder(this).build()
+        val playerView1: StyledPlayerView = findViewById(R.id.player1)
+        playerView1.player = player1
 
-        val mediaItem: MediaItem = MediaItem.fromUri("rtsp://demo:demo@ipvmdemo.dyndns.org:5541/onvif-media/media.amp?profile=profile_1_h264&sessiontimeout=60&streamtype=unicast")
-        player.setMediaItem(mediaItem)
-        player.prepare()
-        player.play()
+        val player2 = SimpleExoPlayer.Builder(this).build()
+        val playerView2: StyledPlayerView = findViewById(R.id.player2)
+        playerView2.player = player2
+
+        val prefs = getSharedPreferences("smoothptzprefs", MODE_PRIVATE)
+
+        val player1urledit: EditText = findViewById(R.id.player1Button)
+        val player1url = prefs.getString("url1", "")!!
+        player1urledit.setText(player1url)
+        play(player1url, player1)
+
+        val player2urledit: EditText = findViewById(R.id.player2Button)
+        val player2url = prefs.getString("url2", "")!!
+        player2urledit.setText(player2url)
+        play(player2url, player2)
+
+        player1urledit.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                prefs.edit {
+                    this.putString("url1", s!!.toString())
+                    this.commit()
+                }
+                play(s!!.toString(), player1)
+                }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+
+        player2urledit.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+//                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                prefs.edit {
+                    this.putString("url2", s!!.toString())
+                    this.commit()
+                }
+                play(s!!.toString(), player2)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+//                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
