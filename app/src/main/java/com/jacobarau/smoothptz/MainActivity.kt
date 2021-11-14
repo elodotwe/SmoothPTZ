15ca61dc2d3e2f6308ce5c6414c0483cde563670
jacob <jarau@mtu.edu>
2021-11-13 20:11:35 -0500

    Play video feeds from all saved sources.
    - Removed Exoplayer in favor of libVLC: Exoplayer refused to stream from
    the PTZOptics cameras at church, whereas VLC proper would, so I'm hoping
    libVLC also can stream from our cameras.
    - We now instantiate a new VLC player + view for each camera that's
    saved in our cameras repository. (Currently no way to edit this
    repository outside of hacking in a `.addCamera()`, see lines 31-32 of
    `MainActivity.kt` for examples with live-as-of-now test RTSP feeds).
    Players are currently arranged in a `LinearLayout` horizontally with
    equal widths, this will cease being optimal for n(cameras) > 3 or so,
    but I don't care as I'm only planning to control 2 ;)

--- app/src/main/java/com/jacobarau/smoothptz/MainActivity.kt
+++ app/src/main/java/com/jacobarau/smoothptz/MainActivity.kt
@@ -1,114 +1,71 @@
 package com.jacobarau.smoothptz
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.Log
-import android.widget.EditText
-import android.widget.TextView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.activity.viewModels
-import androidx.core.content.edit
-import androidx.lifecycle.LiveData
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.lifecycle.asLiveData
-import androidx.lifecycle.lifecycleScope
-import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.ui.StyledPlayerView
+import com.jacobarau.smoothptz.databinding.ActivityMainBinding
+import com.jacobarau.smoothptz.databinding.ActivityMainSingleCameraViewBinding
 import com.jacobarau.smoothptz.settings.Camera
-import com.jacobarau.smoothptz.settings.CameraRepository
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
-import javax.inject.Inject
+import org.videolan.libvlc.LibVLC
+import org.videolan.libvlc.Media
+import org.videolan.libvlc.MediaPlayer
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val mainViewModel: MainViewModel by viewModels()
 
-    fun play(url: String, player: SimpleExoPlayer) {
-        val mediaItem: MediaItem = MediaItem.fromUri(url)
-        player.setMediaItem(mediaItem)
-        player.prepare()
-        player.play()
-    }
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
 
+//        mainViewModel.addCamera(camera = Camera("camera", "rtsp://demo:demo@ipvmdemo.dyndns.org:5541/onvif-media/media.amp?profile=profile_1_h264&sessiontimeout=60&streamtype=unicast"))
+//        mainViewModel.addCamera(camera = Camera("blender test", "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4"))
+
+        setContentView(view)
+    }
+
+    override fun onResume() {
+        super.onResume()
         mainViewModel.cameras.observe(this, Observer { Log.i("foo", "new camera list gotten, $it") })
 
-//        cameras = cameraRepository.cameras.asLiveData()
-//        cameras.observe(this, Observer { Log.i("foo", "new camera list gotten, $it") })
-//
-//        lifecycleScope.launch {
-//            cameraRepository.addCamera(Camera("foo", "bar"))
-//        }
-//        mainViewModel.addCamera(camera = Camera("foo", "bar"))
+        mainViewModel.cameras
+            .observe(this, {
+                binding.noCameraAdded.root.visibility = if (it.isEmpty()) View.VISIBLE else View.GONE
+                binding.camerasLinearLayout.visibility = if (it.isEmpty()) View.GONE else View.VISIBLE
+            })
 
-//        mainViewModel.deleteCamera(0)
-        setContentView(R.layout.activity_main)
-
-        val tv: TextView = findViewById(R.id.tv)
-        tv.text = HelloJNI().foo.toString()
-
-        val player1 = SimpleExoPlayer.Builder(this).build()
-        val playerView1: StyledPlayerView = findViewById(R.id.player1)
-        playerView1.player = player1
-
-        val player2 = SimpleExoPlayer.Builder(this).build()
-        val playerView2: StyledPlayerView = findViewById(R.id.player2)
-        playerView2.player = player2
-
-        val prefs = getSharedPreferences("smoothptzprefs", MODE_PRIVATE)
-
-        val player1urledit: EditText = findViewById(R.id.player1Button)
-        val player1url = prefs.getString("url1", "")!!
-        player1urledit.setText(player1url)
-        play(player1url, player1)
-
-        val player2urledit: EditText = findViewById(R.id.player2Button)
-        val player2url = prefs.getString("url2", "")!!
-        player2urledit.setText(player2url)
-        play(player2url, player2)
-
-        player1urledit.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                prefs.edit {
-                    this.putString("url1", s!!.toString())
-                    this.commit()
+        mainViewModel.cameras
+            .observe(this, { cameras ->
+                binding.camerasLinearLayout.removeAllViews()
+                cameras.forEach { camera ->
+                    binding.camerasLinearLayout.addView(makeCameraView(camera))
                 }
-                play(s!!.toString(), player1)
-                }
+            })
+    }
 
-            override fun afterTextChanged(s: Editable?) {
+    private fun makeCameraView(camera: Camera): View {
+        val cameraViewBinding = ActivityMainSingleCameraViewBinding.inflate(layoutInflater)
+  
+        cameraViewBinding.root.layoutParams = LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1F)
 
-            }
+        val libVLC = LibVLC(this, mutableListOf("-vvv"))
+        val mediaPlayer = MediaPlayer(libVLC)
+        mediaPlayer.attachViews(cameraViewBinding.videoLayout, null, false, false)
+        val media = Media(libVLC, Uri.parse(camera.streamURL))
 
-        })
-
-        player2urledit.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-//                TODO("Not yet implemented")
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                prefs.edit {
-                    this.putString("url2", s!!.toString())
-                    this.commit()
-                }
-                play(s!!.toString(), player2)
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-//                TODO("Not yet implemented")
-            }
-
-        })
+        mediaPlayer.media = media
+        media.release()
+        mediaPlayer.play()
+        return cameraViewBinding.root
     }
 }
