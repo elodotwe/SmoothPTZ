1e87e65343ec81004cfdbe699c6d45c0bd3aefd0
jacob <jarau@mtu.edu>
2022-08-10 23:56:02 -0400

    Add settings action to main frag, direct to settings frag

--- app/src/main/java/com/jacobarau/smoothptz/fragments/MainFragment.kt
+++ app/src/main/java/com/jacobarau/smoothptz/fragments/MainFragment.kt
@@ -1,8 +1,18 @@
 package com.jacobarau.smoothptz.fragments
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
-import androidx.fragment.app.*
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.fragment.findNavController
 import com.jacobarau.smoothptz.MainViewModel
 import com.jacobarau.smoothptz.R
 import com.jacobarau.smoothptz.fragments.main.AllCamerasFragment
@@ -25,5 +35,20 @@ class MainFragment: Fragment(R.layout.main_fragment) {
                 }
             }
         }
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object: MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.main_fragment_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                if (menuItem.itemId == R.id.settings) {
+                    findNavController().navigate(R.id.action_global_settingsFragment)
+                    return true
+                }
+                return false
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 }
